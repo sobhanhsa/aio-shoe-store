8588cd00dd081f106bfd57c15b00afd717cb7e76
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-24 12:13:32 +0330

    feat:fetch size from properties

--- src/components/product-card/ProductCard.tsx
+++ src/components/product-card/ProductCard.tsx
@@ -6,10 +6,12 @@ import Link from "next/link";
 
 type ProductCardParams = {
     brand:      string
-    name:   string
+    name:       string
     image:      string
     prices:      number[]
     colors:     {color:string,price:number}[]
+    sizes:      {size:number,price:number}[]
+    tumbDesc:string
 } & {
     id:string
 };
@@ -20,47 +22,50 @@ const  ProductCard = (params:ProductCardParams) => {
     const highestPrice = Math.max(...params.prices
         .map(p => Number(p)));
     
-    console.log(typeof params.id)
+
+    const priceRange = lowestPrice === highestPrice 
+        ? 
+            lowestPrice 
+        :
+            lowestPrice+"-"+highestPrice
 
     return (
         <Link href={"/"+params.id} key={params.id}>
             <div className={styles.container}>
-                <div className={styles.content}>
-                    <div className={styles.imageContainer}>
-                        <Image 
-                            className={styles.image}
-                            alt=""
-                            src={params.image}
-                            fill
-                        />
+                <div className={styles.imageContainer}>
+                    <Image 
+                        className={styles.image}
+                        alt=""
+                        src={params.image}
+                        fill
+                    />
+                </div>
+                <div className={styles.infoContainer}>
+                    <div className={styles.name}>
+                        {params.name}
                     </div>
-                    <div className={styles.colors}>
+                    <div className={styles.brand}>
+                        {params.brand}
+                    </div>
+                    <div className={styles.sizeContainer}>
                         {
-                            params.colors.map(({color}) => 
-                                (
-                                    <Color color={color} />
-                                )
-                            )
+                            params.sizes.map(size => (
+                                <div key={size.size} className={styles.size}>
+                                    {size.size}
+                                </div>
+                            ))
                         }
+                        
                     </div>
-                    <div className={styles.textContainer}>
-                        <div className={styles.brand}>
-                            {params.brand}
-                        </div>
-                        <div className={styles.name}>
-                            {params.name}
-                        </div>
-                        <div className={styles.price}>
-                            {
-                                lowestPrice === highestPrice ? lowestPrice :
-                                lowestPrice+"-"+highestPrice
-                            
-                            }
-                        </div>
-                    </div>
+                    <p className={styles.price}>
+                        {priceRange}
+                    </p>
+                    <p className={styles.tumbDesc}>
+                        {params.tumbDesc}
+                    </p>
                 </div>
                 <button className={styles.addButton}>
-                    بزن به سبد
+                افزودن به سبد خرید 
                 </button>
             </div>
         </Link> 
