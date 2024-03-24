0f6c1f94f0514c7e54b082f8a05f4fdb62a7b06a
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-24 13:46:02 +0330

    fix:put whole component in content div

--- src/components/saleAd/SaleAd.tsx
+++ src/components/saleAd/SaleAd.tsx
@@ -5,22 +5,24 @@ import Image from "next/image";
 const  SaleAd = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.textContainer}>
-                <p className={styles.title}>
-                بیش از 40% تخفیف نوروزی 
-                </p>
-                <p className={styles.desc}>
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد کتابهای زیادی در شصت و سه درصد گذشته حال و آینده
-                </p>
-                <p className={styles.link}>
-                    <Link href={"/shop"}>
-                        همین حالا خرید کنید       
-                    </Link>
-                </p>
-            </div>
-            <div className={styles.imageContainer}>
-                <Image className={styles.image} 
-                    src={"/haftsin.jpg"} alt="" fill/>
+            <div className={styles.content}>
+                <div className={styles.textContainer}>
+                    <p className={styles.title}>
+                    بیش از 40% تخفیف نوروزی 
+                    </p>
+                    <p className={styles.desc}>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد کتابهای زیادی در شصت و سه درصد گذشته حال و آینده
+                    </p>
+                    <p className={styles.link}>
+                        <Link href={"/shop"}>
+                            همین حالا خرید کنید       
+                        </Link>
+                    </p>
+                </div>
+                <div className={styles.imageContainer}>
+                    <Image className={styles.image} 
+                        src={"/haftsin.jpg"} alt="" fill/>
+                </div>
             </div>
         </div>
 
