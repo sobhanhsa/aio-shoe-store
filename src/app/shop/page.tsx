52efd1d431c1a13d1751d70c0957ef95952de5e6
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-25 11:44:03 +0330

    feat:initialize shop page

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./shopPage.module.css"
+
+const  ShopPage = () => {
+    return (
+        <div className={styles.container}>
+            ShopPage   
+        </div>
+    )
+};
+
+export default ShopPage;
