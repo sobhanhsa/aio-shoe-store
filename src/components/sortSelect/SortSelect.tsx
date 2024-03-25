64ee5de63eaf27ec1f08e985b69d6f288334fb36
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-25 16:11:31 +0330

    feat:add handle change logic

--- src/components/sortSelect/SortSelect.tsx
+++ src/components/sortSelect/SortSelect.tsx
@@ -1,14 +1,39 @@
+"use client"
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import styles from "./sortSelect.module.css"
+import { ChangeEvent } from "react";
 
 const  SortSelect = () => {
+    const {replace} = useRouter();
+    const searchParams = useSearchParams();
+    const path = usePathname();
+    
+    const params = new URLSearchParams(searchParams);
+
+    const param = params.get("sort");
+
+    function handleChange (e:ChangeEvent & any) {
+        const sort = e.target.value;
+        if (param === e.target.value) {
+            params.delete("sort")
+        }else {
+            params.set("sort",sort);
+        }
+        replace(`${path}?${params}`);
+    };
     return (
         <div className={styles.container}>
             <p className={styles.title}>
                 مرتب سازی بر اساس   
             </p>
             <select className={styles.select} 
-                name="sort" id="sort">
+                name="sort" id="sort"
+                onChange={handleChange}
+            >
                 <option value="name">نام</option>
+                <option value="low">ارزان ترین</option>
+                <option value="hight">گران ترین</option>
+                <option value="newest">جدید ترین</option>
             </select>   
         </div>
     )
