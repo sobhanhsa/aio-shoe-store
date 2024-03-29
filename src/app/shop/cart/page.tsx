6928373f934332ed923b295a0212301a5680c510
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-30 01:32:24 +0330

    feat:init cart page

---
+++ src/app/shop/cart/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./cartPage.module.css"
+
+const  CartPage = () => {
+    return (
+        <div className={styles.container}>
+            CartPage   
+        </div>
+    )
+};
+
+export default CartPage;
