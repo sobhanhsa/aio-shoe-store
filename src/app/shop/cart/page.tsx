68c082b52d0e33b8e500c97634f7bc2a4a31734f
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-31 00:01:39 +0330

    feat:make cart item more dynamic

--- src/app/shop/cart/page.tsx
+++ src/app/shop/cart/page.tsx
@@ -6,9 +6,10 @@ import { AiOutlineClose, AiOutlineMinus } from "react-icons/ai";
 import { MdOutlineAdd } from "react-icons/md";
 import { useState } from "react";
 import Image from "next/image";
-import { findCartsByUserId } from "@/utils/db/cartItem/data";
+import { finalCartItem, findCartsByUserId } from "@/utils/db/cartItem/data";
 import { CartItemType } from "@/utils/db/cartItem/model";
 import useSWR from "swr";
+import { commaEmbedder } from "@/utils/priceConventor/priceConventor";
 
 const fetcher = async(url:string) => {
 
@@ -27,12 +28,11 @@ const fetcher = async(url:string) => {
 
 }
 
-
 const  CartPage = () => {
     const [currentQuantity,setCurrentQuantity] = useState(1);
     const {data,isLoading,mutate,error} = useSWR(`${process.env.NEXT_PUBLIC_API_URL}/cart`
     ,fetcher);
-    console.log(error,data);
+    
     return (
         <div className={styles.container}>
             <p className={styles.title}>
@@ -60,38 +60,43 @@ const  CartPage = () => {
                 </p>
             </div>
             <div className={styles.cartsContainer}>
-                <div className={styles.cart}>
-                    <div className={styles.delete}>
-                        <AiOutlineClose style={{cursor:"pointer"}} size={20} color="red"/>
-                    </div>
-                    <div className={styles.imageContainer}>
-                        <Image
-                            src={"/noproduct.jpg"}
-                            alt=""
-                            fill
-                        />
-                    </div>
-                    <p className={styles.product}>
-                        کفش چرمی مردانه
-                    </p>
-                    <p className={styles.price}>
-                        500,000
-                    </p>
-                    <div className={styles.quantity}>
-                        <button onClick={() => {setCurrentQuantity((p) => {
-                            return p === 1 ? 1 : p - 1
-                        })}}>
-                            <AiOutlineMinus size={20} />
-                        </button>
-                        {currentQuantity}
-                        <button onClick={() => setCurrentQuantity(p => p + 1)}>
-                            <MdOutlineAdd size={20}/>
-                        </button>
-                    </div>
-                    <p className={styles.subTotal}>
-                        750,000
-                    </p>
-                </div>
+                {
+                    data?.cartItems?.map((c:finalCartItem) => (
+                        <div className={styles.cart} key={c._id}>
+                            
+                            <div className={styles.delete}>
+                                <AiOutlineClose style={{cursor:"pointer"}} size={20} color="red"/>
+                            </div>
+                            <div className={styles.imageContainer}>
+                                <Image
+                                    src={c.product.images[0].image}
+                                    alt=""
+                                    fill
+                                />
+                            </div>
+                            <p className={styles.product}>
+                                {c.product.name}
+                            </p>
+                            <p className={styles.price}>
+                                {commaEmbedder(c.price)}
+                            </p>
+                            <div className={styles.quantity}>
+                                <button onClick={() => {setCurrentQuantity((p) => {
+                                    return p === 1 ? 1 : p - 1
+                                })}}>
+                                    <AiOutlineMinus size={20} />
+                                </button>
+                                {currentQuantity}
+                                <button onClick={() => setCurrentQuantity(p => p + 1)}>
+                                    <MdOutlineAdd size={20}/>
+                                </button>
+                            </div>
+                            <p className={styles.subTotal}>
+                                {commaEmbedder(c.price * currentQuantity)}
+                            </p>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
