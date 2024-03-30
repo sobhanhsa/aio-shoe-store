8bb9d4002d96830c9bc51ccd299c8fa2ddfa6862
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-30 17:29:12 +0330

    feat:add slug

--- src/utils/db/cartItem/model.ts
+++ src/utils/db/cartItem/model.ts
@@ -2,25 +2,34 @@ import mongoose, { InferSchemaType, Schema } from "mongoose";
 
 
 const cartItemSchema = new Schema({
-    productId:{
-        required:true,
-        type:mongoose.Types.ObjectId
+    slug:{
+        unique:true,
+        requirled:true,
+        type:String
     },
     spec :{
         required:true,
-        colorName:{
-            type:String,
-            required:true
-        },
-        Size:{
-            type:Number,
-            required:true
+        type:{
+            _id:false,
+            colorName:{
+                type:String,
+                required:true
+            },
+            size:{
+                type:Number,
+                required:true
+            },
+            userId:{
+                required:true,
+                type:mongoose.Types.ObjectId
+            },
+            productId:{
+                required:true,
+                type:mongoose.Types.ObjectId
+            },
         }
     },
-    userId:{
-        required:true,
-        type:mongoose.Types.ObjectId
-    },
+    
     quantity:{
         required:true,
         type:Number
