9ce0eceed1b101c9f962a65a63a333ebc3e5ae10
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-31 00:01:09 +0330

    feat:embed price and product

--- src/utils/db/cartItem/data.ts
+++ src/utils/db/cartItem/data.ts
@@ -2,14 +2,40 @@ import { connectToDB } from "@/utils/db/utils";
 import { CartItemModel, CartItemType } from "./model";
 import { slugMaker } from "./utils";
 import mongoose from "mongoose";
+import { findCartShoe, findShoe } from "@/utils/db/shoe/data";
+import { ShoeType } from "../shoe/model";
+
+export type finalCartItem = CartItemType & {
+    product:ShoeType,
+    price:number
+}
 
 export const findCartsByUserId = async(userId:string) => {
     try {
         await connectToDB();
-        const cartItems : CartItemType[] = await CartItemModel.find({
+        const cartItems :  CartItemType[] = await CartItemModel.find({
             'spec.userId':new mongoose.Types.ObjectId(userId)
         });
-        return cartItems
+        const finalCartItems = await Promise.all(cartItems.map(async(cI) => {
+            const product : ShoeType = await findCartShoe(cI.spec.productId);
+            const price =  product.prices.find(
+                p => {
+                    // console.log(p);
+                    return(
+                        p.colors.find(c => c.colorName === cI.spec.colorName) 
+                        && 
+                        p.sizes.find(s => s.size === cI.spec.size)
+                    )
+                }
+            )?.price;
+            return ({
+            ...(cI as any)._doc,
+            product,
+            price
+        })}));
+        
+        // console.log(finalCartItems);
+        return finalCartItems
     } catch (err:Error&any) {
         throw err;
     }
