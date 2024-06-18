ec926dd9656f95e7c9e95b3c9d0b7c7363f0dd5d
sobhanhsa <sobhanb7b7@protonmail.com>
2024-06-18 20:04:27 +0330

    feat : define signup page

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,85 @@
+"use client"
+
+import { FormEvent, ReactNode, useState } from "react"
+import styles from "./signupPage.module.css"
+import { useSignIn } from "@/hooks/signIn"
+
+export const SignUpPage = () => {
+
+    type keysType = "email" | "password"
+
+    const [formInfo,setFormInfo] = useState({
+        email:"",
+        password:"",
+        username:"",
+        name:""
+    });
+
+    const onSubmit = (e?:FormEvent) => {
+
+        e?.preventDefault();
+
+        console.log("auth SignInPage credentials : ",formInfo);
+        
+        
+
+        useSignIn(formInfo);
+
+    }
+
+    const onInputChange = (e:any) => {
+        setFormInfo(prev  => {
+            prev[e.target.name as keysType] = e.target.value;
+            return {
+                ...prev
+            }
+        })
+    }
+
+    return (
+        <div className={styles.container}>
+            <form className={styles.form} onSubmit={onSubmit}>
+                <h1>
+                    به کفش خونه خوش اومدی
+                </h1>
+                <input className={styles.input} 
+                    onChange={onInputChange}
+                    type="email"
+                    name="email" 
+                    required
+                    placeholder="ایمیل"
+                />
+                <input className={styles.input} 
+                    onChange={onInputChange}
+                    type="text"
+                    name="username" 
+                    required
+                    placeholder="نام کاربری"
+                />
+                <input className={styles.input} 
+                    onChange={onInputChange}
+                    type="text"
+                    name="name" 
+                    required
+                    placeholder="نام"
+                />
+                <input className={styles.input}
+                    onChange={onInputChange}
+                    type="password"
+                    name="password" 
+                    required
+                    placeholder="رمز عبور"
+                />
+                <button className={styles.button} onClick={onSubmit}>
+                    ورود
+                </button>
+            </form>
+            <div className={styles.img}>
+
+            </div>
+        </div>
+    )
+}
+
+export default SignUpPage
+
