e01c7d44fa7cf2fd54cd36b3bc818c8af4e3a9ac
sobhanhsa <sobhanb7b7@protonmail.com>
2024-06-18 20:13:07 +0330

    docs : log more clear

--- src/utils/db/utils.ts
+++ src/utils/db/utils.ts
@@ -10,10 +10,15 @@ export async function connectToDB() {
     try {        
         if (connection.isConnected) return;
         const db = await mongoose.connect(process.env.DB_URl as string);
-        console.log("db initialized");
+        console.log("utils connectToDB : db initialized");
         connection.isConnected = db.connections[0].readyState;
-        console.log(connection.isConnected);
+        console.log(
+            "utils connectToDB connections : ",
+            connection.isConnected
+        );
     } catch(error:any) {
         throw new Error(error);
     }
 };
+
+
