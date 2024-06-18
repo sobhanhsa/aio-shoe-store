5501fc66813762cf7ea5b4e64e3d5ce6e313b8fe
sobhanhsa <sobhanb7b7@protonmail.com>
2024-06-18 20:11:12 +0330

    feat : add hoooks

---
+++ src/hooks/signIn.ts
@@ -0,0 +1,66 @@
+import { redirect, useRouter } from "next/navigation";
+import { render } from "react-dom";
+import { toast } from "react-toastify";
+
+export const useSignIn = async(credentials:any) => {
+    
+    // fetch
+    const res = await fetch(
+        process.env.NEXT_PUBLIC_API_URL+"auth/login",{
+        method:"POST",
+        body:JSON.stringify(
+            credentials
+        ),
+        }
+    );
+
+    console.log("hooks useSignIn res : ",res);
+    
+
+    // toast the result
+
+    const toastRes = new Promise(async(resolve,reject) => {
+            if (res.ok) {
+                resolve("خوش امدید");
+            }
+
+            const body : {message:string} = await res.json();
+        
+            switch (body.message) {
+                case "must be 5 or more characters long":
+                    reject("رمز عبور حداقل باید پنچ حرف باشد")
+                case "invalid email" :
+                    reject("ایمیل اشتباه");
+                case "you are already logged in" :
+                    reject("شما در حال حاضر وارد شده اید");
+                case "email does not match":
+                    reject("ایمیل ثبت نشده");
+                case "incorrect password":
+                    reject("رمز عبور اشتباه");
+                default :
+                    reject("شرمساریم!مشکلی رخ داد");
+                
+            }
+        }
+    );
+
+    toast.promise(toastRes,{
+        pending:"در حال اعتبار سنجی",
+        success:{
+            render({data}){
+                return `${data}`
+            }
+        },
+        error:{
+            render({data}){
+                return `${data}`
+            }
+        },
+    });
+
+    // redirect
+
+    useRouter().refresh();
+
+
+}
