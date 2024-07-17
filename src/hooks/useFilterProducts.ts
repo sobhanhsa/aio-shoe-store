ee15fcbd8052c40e43ef6623bc59d01447773b2b
sobhanhsa <sobhanb7b7@protonmail.com>
2024-07-17 18:46:23 +0330

    feat : add count and use raw query

--- src/hooks/useFilterProducts.ts
+++ src/hooks/useFilterProducts.ts
@@ -6,24 +6,30 @@ import { useState } from "react"
 
 
 
-export const useFilterProducts = (setProducts:Function) => {
+export const useFilterProducts = (
+    setProducts:Function,setPageCount:Function
+) => {
 
     const [isLoading,setIsLoading] = useState<Boolean>(false);
 
     const fetcher = async(
         url=process.env.NEXT_PUBLIC_API_URL+"products/product/all",
-        filter?:string
+        filter?:string,
+        rawQueries?:object
     )=>{
 
         try {
             
             setIsLoading(true);
             
-            console.log("filter",filter);
-            
+            const finalUrl = new URL(url);    
 
-            const finalUrl = new URL(url);
+            // finalUrl.searchParams.append("page",(page ?? 1).toString());
             
+            Object.entries(rawQueries || {})?.forEach(([key,prop],i)=>{
+                finalUrl.searchParams.append(key,(prop).toString());
+            });
+
             if (filter) {
                 finalUrl.searchParams.append(
                     "filter",filter||"{}"
@@ -34,10 +40,11 @@ export const useFilterProducts = (setProducts:Function) => {
                 finalUrl.toString()
             );
         
-            const body = await res.json();
+            const {products,pageCount} = await res.json();
     
-            setProducts(body.products);
-    
+            setProducts(products || []);
+            setPageCount(pageCount);
+
             setIsLoading(false);
             
         } catch (error:any) {
